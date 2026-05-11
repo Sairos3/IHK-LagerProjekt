8e082cd2dae352b0d0d11a9c82f4424b4c51713b
sairos3 <sairos3@live.com>
2026-05-11 09:42:31 +0200

    Remove allowed hosts all from testing

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f_6$aa5d!fo_u)vukxg-jn!@kfdv%!q21g1gutzz#e*g8c%gau
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
